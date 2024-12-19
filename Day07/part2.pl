5b961ae10bad3c2f0ae2eb435e138a6050260d5e
Kip Landwehr <klandwehr@live.com>
2024-12-19 17:48:57 -0600

    Add template and d7

---
+++ Day07/part2.pl
@@ -0,0 +1,49 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+my $debug = 0;
+my $total = 0;
+
+while (<>) {
+	chomp;
+
+	my ($target, $vals) = split ": ";
+	my @values = split " ", $vals;
+
+	my $start = shift @values;
+
+	if (isValid($target, $start, @values)) {
+		$total += $target;
+	}
+}
+
+# Print solution
+print "Solution: $total\n";
+
+sub isValid {
+	my ($target, $runningTotal, @values) = @_;
+
+	if (@values == 0) {
+		return 1 if ($runningTotal == $target);
+		return 0;
+	}
+	if ($runningTotal > $target) {
+		return 0;
+	}
+
+	my $value = shift @values;
+
+	if (isValid($target, $runningTotal*$value, @values)) {
+		return 1;
+	}
+	elsif (isValid($target, $runningTotal+$value, @values)) {
+		return 1;
+	}
+	elsif (isValid($target, $runningTotal.$value, @values)) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
---
+++ template.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+#use File::Basename qw(dirname);
+#use Cwd qw(abs_path);
+#use lib dirname(dirname abs_path $0);
+#use My::AoC::Helper qw(coordValid getDirOffsetCoord);
+
+my $debug = 0;
+my $total = 0;
+
+# Input separator
+# $/ = "\n";
+while (<>) {
+	chomp;
+}
+
+# Print solution
+print "Solution: $total\n";
